7e33d2bd772bfdd4499bc7920020caf77ddf6506
github kyleyang <kxy156@case.edu>
2017-12-10 10:34:43 -0700

    fix -1

--- Lecture3/Concentration-in-class/Concentration/ViewController.swift
+++ Lecture3/Concentration-in-class/Concentration/ViewController.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
     lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1)/2)
     
     @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
-    
-    @IBOutlet weak var scoreLabel: UILabel!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
@@ -29,25 +29,6 @@ class ViewController: UIViewController {
         else {
             print("Warning! The chosen card was not in cardButtons")
         }
-    }
-    
-    @IBAction func newGame() {
-        flipCount = 0
-        game.touchScore = 0
-        
-        for cardNumber in game.cards.indices {
-            game.cards[cardNumber].isFaceUp = false
-            game.cards[cardNumber].isMatched = false
-            game.cards[cardNumber].isSeen = false
-        }
-        
-        emoji = [:]
-        
-        let themeIndex = Int(arc4random_uniform(UInt32(emojiChoicesOrigin.count)))
-        
-        emojiChoices = emojiChoicesOrigin[themeIndex]
-        
-        updateViewFromModel()
     }
     
     
@@ -68,38 +49,19 @@ class ViewController: UIViewController {
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
         }
-        
-        scoreLabel.text = "Score: \(game.touchScore)"
-        
     }
     
-    let emojiChoicesOrigin: [[String]] = [
-        //face
-        ["😀", "😃", "😄", "🤓", "😂", "😊","🤑","😖"],
-        //animal
-        ["🦊","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐶"],
-        //cloth
-        ["👚","👕","👖","👔","👗","👙","👘","👠","👡"],
-        //fruit
-        ["🍏","🍎","🍐","🍊","🍋","🍉","🍌","🍇","🍓"],
-        //vhiecle
-        ["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒"],
-        //flag
-        ["🇦🇫","🇧🇸","🇧🇳","🇨🇰","🇨🇱","🇨🇦","🇧🇷","🇭🇷","🇯🇵"]
-        ]
-    
-    var emojiChoices: [String] = []
+    var emojiChoices = ["🐶","🐱", "🐭","🐹","🐰","🦊","🐻","🐼","🐨","🐯","🦁","🐮", "🐷"]
     
     var emoji = [Int: String]()
     
     func emoji(for card: Card) -> String {
-        if emojiChoices.count == 0{
-           let themeIndex = Int(arc4random_uniform(UInt32(emojiChoicesOrigin.count)))
-           emojiChoices = emojiChoicesOrigin[themeIndex]
-        }
+        
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            
             emoji[card.identifier] = emojiChoices[randomIndex]
+            
             emojiChoices.remove(at: randomIndex)
         }
         
@@ -112,4 +74,5 @@ class ViewController: UIViewController {
         }
     }
     
+
 }
